9be5b0f244186e82b980febc5149822d030f1bb0
EllipticElysium <secondary@aidan-byrne.org.uk>
2024-02-03 01:23:34 +0000

    example permission checking middleware

--- src/routers/Endpoints.ts
+++ src/routers/Endpoints.ts
@@ -3,6 +3,13 @@ import { cookie } from "@elysiajs/cookie";
 import { AuthPlugin } from "./pugins";
 import { SessionManager } from "../helpers";
 
+type User = {
+    given_name: string;
+    family_name: string;
+    email: string;
+    id: string;
+};
+
 const EndpointRoutes = (sessionManager: SessionManager) =>
     new Elysia()
         .use(cookie())
@@ -14,6 +21,7 @@ const EndpointRoutes = (sessionManager: SessionManager) =>
             return "World!";
         })
         .get("/user", ({ user }) => {
+            // user is set by the AuthPlugin
             return `Hello ${user.given_name} ${user.family_name}!`;
         })
         .get("/session", ({ session }) => {
@@ -22,6 +30,23 @@ const EndpointRoutes = (sessionManager: SessionManager) =>
                 sessionId: session.sessionId,
                 user: session.getSessionItem("user"),
             };
-        });
+        })
+        .get(
+            "/session-protected",
+            ({ session }) => {
+                return {
+                    sessionId: session.sessionId,
+                    user: session.getSessionItem("user"),
+                };
+            },
+            // we can also protect routes with middleware
+            { beforeHandle: ({ user }) => checkPermissions(user, "read:session") },
+        );
+
+function checkPermissions(user: User, permission: string) {
+    if (permission === "read:session") {
+        throw new Error(`User ${user.given_name} does not have permission to read:session`);
+    }
+}
 
 export default EndpointRoutes;
